356b7c25ce9a8996ecf21f5fd0166755d652c93c
ashis-solomon <ashissolomon24@gmail.com>
2022-10-19 14:27:53 +0530

    explicit and key mapping

--- static/js/stat.js
+++ static/js/stat.js
@@ -53,13 +53,48 @@ var D2 = document.getElementById('D2');
 var D3 = document.getElementById('D3');
 var D4 = document.getElementById('D4');
 var D5 = document.getElementById('D5');
+var exp = document.getElementById('exp');
+console.log(exp)
 
 // D1.innerHTML = `Duration : ${localStorage.getItem('duration')/60000} min ${localStorage.getItem('duration')%60000} sec`
 D1.innerHTML = `Duration : ${convertMsToMinutesSeconds(localStorage.getItem('duration'))}`
 D2.innerHTML = `Tempo : ${localStorage.getItem('tempo')}`
 D3.innerHTML = `Explicit : ${localStorage.getItem('trackExplicit')}`
+if(localStorage.getItem('trackExplicit') == 'true'){
+      exp.querySelector("img").style = "";
+}else{
+      exp.querySelector("img").style = "visibility: hidden";
+}
 D4.innerHTML = `Loudness : ${localStorage.getItem('loudness')}`
-D5.innerHTML = `Key : ${localStorage.getItem('key')}`
+
+var dict = {
+  0 : "C",
+  1 : "C#",
+  // 1 : "C♯ / D♭",
+  2 : "D",
+  3 : "D#",
+  // 3 : "D♯ / E♭",
+  4 : "E",
+  5 : "F",
+  6 : "F#",
+  // 6 : "F♯ / G♭",
+  7 : "G",
+  8 : "G#",
+  // 8 : "G♯ / A♭",
+  9 : "A",
+  10 : "A#",
+  // 10 : "A♯ / B♭",
+  11 : "B",
+
+}
+
+let minmajor = "";
+if(localStorage.getItem('mode')==0){
+  minmajor = " Minor"
+}else{
+  minmajor = " Major"
+}
+D5.innerHTML = `Key : ${dict[localStorage.getItem('key')]+minmajor}`
 
 
 
